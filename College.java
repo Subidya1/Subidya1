f0a9611fc722774674941845114b9adcf350592c
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:02:00 +0000

    Add files via upload

---
+++ College.java
@@ -0,0 +1,17 @@
+package org.College;
+
+public class College {
+
+	public void CollegeName() {
+		System.out.println("CollegeName is KIIT");
+	}
+
+	public void CollegeCode() {
+		System.out.println("CollegeCode is 12345");
+	}
+	public void CollegeRank() {
+		System.out.println("CollegeRank is 1");
+	}
+	
+}
+
