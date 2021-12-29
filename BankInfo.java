f922f0df2e38480524879aff87c7dd1498b3ae28
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:20:45 +0000

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,16 @@
+package week3.day1.Assignments;
+
+public class BankInfo {
+
+	public void Saving() {
+		System.out.println("This is Saving Account");
+	}
+	
+	public void Fixed() {
+		System.out.println("This is Fixed Account");
+	}
+	
+	public void Deposite() {
+		System.out.println("This is Deposite Account");
+	}
+}
