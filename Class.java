56cef1e98db28eb80e7c67939e4dbc9527eac48c
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:11:17 +0000

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,23 @@
+package week1.day2.Assignments;
+public class Class {
+	public static void main(String[] args) {
+
+		String x = "abc";
+		String y = "abc";
+		x.concat(y);
+		System.out.print(x);
+		}
+		
+		}
+
+		
+		
+
+		
+
+	
+	
+
+	
+		
+		
