7e3d9088884393db4535d4e529a2e698c6675b0a
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:09:53 +0000

    Add files via upload

---
+++ Calculator1.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Calculator1 {
+	public double mul(double num1 , double num2) {
+		return  num1*num2;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator1 calc = new Calculator1();
+
+		System.out.println(calc.mul(5.5, 3.4));
+	}
+
+}
+
