c007d75e55a96542f42c47fab929196da4a59236
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:10:12 +0000

    Add files via upload

---
+++ Calculator2.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Calculator2 {
+	public int divide(int num1 , int num2) {
+	      return num1/num2;
+       
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator2 calc = new Calculator2();
+
+		System.out.println(calc.divide (30,5));
+	}
+
+}
