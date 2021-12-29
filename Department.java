aecc74f401f47583d99d34394a8b8abc0c64cefa
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:02:53 +0000

    Add files via upload

---
+++ Department.java
@@ -0,0 +1,12 @@
+package org.Department;
+
+import org.College.College;
+
+public class Department extends College {
+
+	public void DepartmentName()
+	{
+		System.out.println("Department is ComputerScience");
+	}
+
+}
