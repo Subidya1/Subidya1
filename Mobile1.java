1a5323ca9439b367359ba3a4ed61a0460d64344a
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:08:11 +0000

    Add files via upload

---
+++ Mobile1.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Mobile1 {
+	String mobileBrand = "Apple";
+	int mobileWeight = 1;
+	boolean isFullcharged = true;
+	int mobileCost = 50000;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Mobile1 mob = new Mobile1();
+System.out.println(mob.mobileBrand);
+System.out.println(mob.mobileWeight);
+System.out.println(mob.isFullcharged);
+System.out.println(mob.mobileCost);
+
+	}
+
+}
