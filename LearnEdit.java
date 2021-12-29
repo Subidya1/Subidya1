129c383da10d6813310ef1845726a1e4cc7b9407
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:14:50 +0000

    Add files via upload

---
+++ LearnEdit.java
@@ -0,0 +1,24 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnEdit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leafground.com/pages/Edit.html");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("email")).sendKeys("Subidya.s.Pradhan@gmail.com");
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys("Pinky" ,Keys.TAB);
+	}
+
+}
