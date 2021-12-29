e7235084c4a4d38c690d0f9b0103a9c12874b33e
Subidya1 <95372509+Subidya1@users.noreply.github.com>
2021-12-29 22:20:02 +0000

    Add files via upload

---
+++ MyPhone.java
@@ -0,0 +1,15 @@
+package week3.day1;
+
+public class MyPhone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ SmartPhone sp = new SmartPhone();
+ sp.SendMsg();
+ sp.MakeCall();
+ sp.SaveContact();
+ sp.TakeVideo();
+ sp.ConnectWhatsApp();
+	}
+
+}
